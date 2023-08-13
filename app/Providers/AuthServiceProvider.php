f681e401609ccf9f9ff0147b24533ebe01b23206
namumakwembo <makwembonamu@gmail.com>
2023-08-13 20:24:10 +0300

    added env host to braodcasting

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,10 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -22,5 +25,9 @@ class AuthServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+
+        Gate::define('viewWebSocketsDashboard', function ($user = null) {
+            return $user != null;
+        });
     }
 }
