15ca79875984435a21ff2137e3c560d5b24a7940
namumakwembo <makwembonamu@gmail.com>
2023-08-13 20:37:32 +0300

    asdfj;

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -56,13 +56,9 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => true,
-                'host' => '127.0.0.1',
+                'host' => env('PUSHER_HOST'),
                 'port' => env('PUSHER_PORT', 443), 
                 'scheme' => env('PUSHER_SCHEME', 'https'),
-                'curl_options' => [
-                    CURLOPT_SSL_VERIFYHOST => 0,
-                    CURLOPT_SSL_VERIFYPEER => 0,
-                ]
             ],
         ],
 
